9d8534a6ca2d23406d027d6f5c3bfffecf51ac66
dev_TK <tomaszkrupa95@gmail.com>
2018-12-16 14:19:33 +0100

    Added first lesson

---
+++ VerySeriousApp/VerySeriousApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VerySeriousApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my first .net core app!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            // Concatenation by multiple Write method call.
+            Console.Write("It's nice to meet you, ");
+            Console.Write(name);
+            Console.Write(Environment.NewLine);
+
+            // Concatenation by '+'
+            Console.WriteLine("It's nice to meet you, " + name);
+
+            // Concatenation by string.Concat
+            Console.WriteLine(string.Concat("It's nice to meet you, ", name));
+
+            // Concatenation by WriteLine args
+            Console.WriteLine("It's nice to meet you, {0}", name);
+
+            // Concatenation by new .net standard
+            Console.WriteLine($"It's nice to meet you, {name}");
+
+            Console.ReadLine();
+        }
+    }
+}
